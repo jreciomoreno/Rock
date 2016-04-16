992907f50c939e99153f5b5a0abce33d19eed59d
edmistj <jon@sparkdevnetwork.org>
2016-04-15 19:28:39 -0700

    - initial mef index components

---
+++ Rock/UniversalSearch/IndexComponents/ElasticSearch.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rock.Attribute;
+
+namespace Rock.UniversalSearch.IndexComponents
+{
+    [Description( "ElasticSearch Universal Search Index" )]
+    [Export( typeof( IndexComponent ) )]
+    [ExportMetadata( "ComponentName", "ElasticSearch" )]
+
+    [TextField( "Node Url", "The URL of the ElasticSearch node (http://myserver:9200)", true)]
+    public class ElasticSearch : IndexComponent
+    {
+    }
+}
